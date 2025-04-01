d388a46e29fb181b2fac9bb1ca250efe8efdcfd3
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-04-01 22:56:32 +0200

    view tasks on page

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let addTaskForm = document.querySelector(".container");
-let taskDisc = document.querySelector("#taskDescription");
+let taskTitle = document.querySelector("#taskTitle");
 let taskDueDate = document.querySelector("#due");
-
 let addTaskBtn = document.querySelector(".addTaskBtn");
+let todoList = document.querySelector(".todoList");
 
 let tasks = [];
+
+
+
 
 function viewTaskForm(){
     addTaskForm.style.display = "contents";
@@ -14,10 +17,34 @@ function viewTaskForm(){
 addTaskBtn.addEventListener("click", (event)=>{
     //prevent adding tasks if the value is none and tell user to fix his input
     event.preventDefault();
-    let taskDiscVal = taskDisc.value;
+    let taskTitleVal = taskTitle.value;
     let taskDueDateVal = taskDueDate.value;
-    tasks.push({taskDiscVal, taskDueDateVal});
-    console.log(tasks);
+    tasks.push({taskTitleVal, taskDueDateVal});
+    addTaskToPage(taskDueDateVal, taskTitleVal);
     addTaskForm.style.display = "none";
 
 })
+
+
+function addTaskToPage(dueDate, taskTitle)
+{
+    let todoItemTag = `
+    <div class="todoItem">
+                <div>
+                    <button>Done</button>
+                </div>
+                <div>
+                
+                    <p class="todoItemDue">${dueDate}</p>
+                    <p class="todoItemTitle">${taskTitle}</p>
+               
+            </div>
+                <div>
+                    <button>Delete</button>
+                </div>
+                
+            </div>
+    `;
+
+    todoList.innerHTML += todoItemTag;
+}
