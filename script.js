fe72693621ea83582e5b522451d34bf70235eace
BasmalaMohamedd <basmalamohamed934@gmail.com>
2025-04-01 21:17:18 +0200

    add tasks to tasks array

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let addTaskForm = document.querySelector(".container");
+let taskDisc = document.querySelector("#taskDescription");
+let taskDueDate = document.querySelector("#due");
 
+let addTaskBtn = document.querySelector(".addTaskBtn");
+
+let tasks = [];
 
 function viewTaskForm(){
     addTaskForm.style.display = "contents";
 
 }
 
+addTaskBtn.addEventListener("click", (event)=>{
+    //prevent adding tasks if the value is none and tell user to fix his input
+    event.preventDefault();
+    let taskDiscVal = taskDisc.value;
+    let taskDueDateVal = taskDueDate.value;
+    tasks.push({taskDiscVal, taskDueDateVal});
+    console.log(tasks);
+    addTaskForm.style.display = "none";
+
+})
